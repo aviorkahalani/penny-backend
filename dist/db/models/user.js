63f358276cbdff6f719ef78c14fba2b2e4472e5d
Avior Kahalani <aviorkahalani@gmail.com>
2025-11-06 19:06:06 +0200

    refactor: order of fields

--- dist/db/models/user.js
+++ dist/db/models/user.js
@@ -43,17 +43,17 @@ const schema = new mongoose_1.Schema({
     email: {
         type: String,
         required: [true, 'email is required'],
-        lowercase: true,
         unique: [true, 'email is already taken'],
         match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'invalid email pattern'],
+        lowercase: true,
         index: true,
     },
     name: {
         type: String,
         required: [true, 'name is required'],
-        trim: true,
         minLength: [2, 'name must be at least 2 characters'],
         maxLength: [30, 'name must not exceed 50 characters'],
+        trim: true,
     },
     password: {
         type: String,
