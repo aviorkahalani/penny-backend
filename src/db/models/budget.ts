96fd0dff80034161f465ba5facab45d6012f6166
Avior Kahalani <aviorkahalani@gmail.com>
2025-11-14 16:14:30 +0200

    feat: add budget model

---
+++ src/db/models/budget.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Types } from 'mongoose'
+
+interface IBudget {
+  userId: Types.ObjectId
+  date: {
+    year: number
+    month: number
+  }
+  currency: 'NIS' | 'USD'
+  notes: string
+}
+
+const schema = new Schema<IBudget>({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  date: {
+    year: { type: Number, required: true },
+    month: { type: Number, required: true, min: 1, max: 12 },
+  },
+  currency: {
+    type: String,
+    default: 'NIS',
+  },
+  notes: {
+    type: String,
+    maxLength: 120,
+  },
+})
+
+export const Budget = mongoose.model('budget', schema)
