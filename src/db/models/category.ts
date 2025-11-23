549d08c6061961454df1a3211f31973cc59af6f3
Avior Kahalani <aviorkahalani@gmail.com>
2025-11-23 15:58:07 +0200

    feat: add category model

---
+++ src/db/models/category.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Types } from 'mongoose'
+
+export interface ICategory {
+  userId: Types.ObjectId
+  budgetId: Types.ObjectId
+  type: 'income' | 'expense' | 'saving'
+  name: string
+  plannedAmount: number
+  createdAt: Date
+  updatedAt: Date
+}
+
+const schema = new Schema<ICategory>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    budgetId: {
+      type: 'ObjectID',
+      ref: 'Budget',
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    plannedAmount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+)
+
+export const Category = mongoose.model('Category', schema)
